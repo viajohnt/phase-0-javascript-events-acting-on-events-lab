b8254f2e36ba0e7d2fd0eca2f406d9a2290e77e4
John Truong <viajohnt@gmail.com>
2023-01-05 23:31:35 -0700

    commit tree 237b01622cb0a92acac77bd3c32eff885dcf763f

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Your code here
+const dodger = document.getElementById('dodger');
+
+function moveDodgerLeft() {
+  const left = parseInt(dodger.style.left, 10);
+  dodger.style.left = `${left - 10}px`;
+}
+
+function moveDodgerRight() {
+  const left = parseInt(dodger.style.left, 10);
+  dodger.style.left = `${left + 10}px`;
+}
+
+document.addEventListener('keydown', event => {
+  if (event.code === 'ArrowLeft') {
+    moveDodgerLeft();
+  } else if (event.code === 'ArrowRight') {
+    moveDodgerRight();
+  }
+});
